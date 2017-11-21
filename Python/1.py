c42653e346bbde2a511da706c6a63b2c3d50fb89
Eviless <183178188@qq.com>
2017-11-21 16:05:17 +0800

    Eviless

--- Python/1.py
+++ Python/1.py
@@ -4,7 +4,6 @@ def triangles1(n):
     while n>0:
         yield T
         T.append(0)
-        print(T)
         T = [T[i]+T[i-1] for i in range(len(T))]
         n = n - 1
 n = int(input('输入杨辉三角层数:\n'))
