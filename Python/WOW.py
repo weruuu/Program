44e159383f2869fad947d840aebe480d96a91176
Eviless <183178188@qq.com>
2018-07-21 10:09:25 +0800

    Revert "Eviless"
    This reverts commit 129fc5be44cc1b2d3fbb0d92038c509776d98a24.

--- Python/WOW.py
+++ Python/WOW.py
@@ -5,10 +5,8 @@ import time
 
 searchitem = '抑魔金' #input()
 
-
 headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
 url = 'https://api.bnade.com/items?name='+searchitem
-      # +'&realm='
 web_data = requests.get(url,headers = headers)
 datas = json.loads(web_data.text)
 results = datas[0]['id']
@@ -19,4 +17,4 @@ datas = json.loads(web_data.text)
 #soup = BeautifulSoup(web_data.text,'lxml')
 
 for i in datas:
-    print(i['owner'],i['buyout']/10000,i['quantity'],i['timeLeft'])
+    print(i)
