39c5af02ccd60eebf79ea4ba8eadb4d5204544fc
Eviless <183178188@qq.com>
2022-01-06 14:49:14 +0800

    Eviless

---
+++ Python/Max_singleletter_subsets1.py
@@ -0,0 +1,17 @@
+#子集遍历
+a= input()
+maxl = []
+max = 0
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        flag = 0
+        for k in a[i:j+1]:
+            if k.isdigit() == False:
+                flag += 1
+            if flag > 1:
+                break
+        if flag == 1 and len(a[i:j+1])>max and len(a[i:j+1])!=1:
+            maxl = a[i:j+1]
+            max = len(a[i:j+1])
+print(maxl,max)
+
--- Python/Max_singleletter_subsets2.py
+++ Python/Max_singleletter_subsets2.py
@@ -1,20 +1,3 @@
-#子集遍历
-a= input()
-maxl = []
-max = 0
-for i in range(len(a)):
-    for j in range(i,len(a)):
-        flag = 0
-        for k in a[i:j+1]:
-            if k.isdigit() == False:
-                flag += 1
-            if flag > 1:
-                break
-        if flag == 1 and len(a[i:j+1])>max and len(a[i:j+1])!=1:
-            maxl = a[i:j+1]
-            max = len(a[i:j+1])
-print(maxl,max)
-
 #单循环
 a= input()
 index,flag = -1,0
