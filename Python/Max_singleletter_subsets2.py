a947fab4c00a362129957884324977092c04d6be
Eviless <183178188@qq.com>
2022-02-09 16:06:45 +0800

    Eviless

--- Python/Max_singleletter_subsets2.py
+++ Python/Max_singleletter_subsets2.py
@@ -1,5 +1,30 @@
+# #单循环
+# a= '12as67865o254'#input()
+# index,flag,stridx = -1,0,0
+# anow,amax = [],[]
+# for i in a:
+#     if i.isdigit() == True:
+#         anow.append(i)
+#     elif i.isdigit() == False and index == -1:
+#         anow.append(i)
+#         index = anow.index(i)
+#         stridx = a.index(i)
+#         flag = 1
+#     else :
+#         anow.append(i)
+#         anow = anow[stridx+1:]
+#         stridx = 0
+#     if len(amax) <= len(anow):
+#         amax = anow.copy()
+#     else :
+#         index = anow.index(i)
+#     if (len(amax) == 1 and amax[0].isdigit() == False) or flag == 0:
+#         amax = []
+# print(amax)
+
+
 #单循环
-a= '1234s56fad'#input()
+a= '11a11a211a1115a5a4a'
 index,flag = -1,0
 anow,amax = [],[]
 for i in a:
@@ -12,10 +37,11 @@ for i in a:
     else :
         anow.append(i)
         anow = anow[index+1:]
+        index = anow.index(i)
     if len(amax) < len(anow):
         amax = anow.copy()
-    else :
-        index = anow.index(i)
+    #else :
+        #index = anow.index(i)
     if (len(amax) == 1 and amax[0].isdigit() == False) or flag == 0:
         amax = []
 print(amax)
